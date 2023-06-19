415c46f22da449a3567713bfcace7a8fcc0e0b9c
benjamin.gueffier <benjamin.gueffier@edu.esiee.fr>
2023-06-19 13:11:21 +0200

    Health Goal Page - Module 2 END

---
+++ src/components/ModalHealthGoal.js
@@ -0,0 +1,67 @@
+import { Text, Button } from '@react-native-material/core';
+import React from 'react';
+import { StyleSheet, Modal, View } from 'react-native';
+
+const ModalHealthGoal = ({ modalVisible, setModalVisible, bmr }) => {
+  return (
+    <View style={styles.centeredView}>
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Your BMR (Basal Metabolic Rate) is {bmr}</Text>
+            <Button
+              style={styles.inputText}
+              onPress={() => setModalVisible(false)}
+              title="Close modal"
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+export default ModalHealthGoal;
--- src/screens/HealthGoalsScreen.js
+++ src/screens/HealthGoalsScreen.js
@@ -1,88 +1,163 @@
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import { TextInput, Text, Button } from '@react-native-material/core';
 import { Picker } from '@react-native-picker/picker';
-import React from 'react';
-import { SafeAreaView, StatusBar, ScrollView, StyleSheet, Platform } from 'react-native';
+import React, { useState } from 'react';
+import ModalHealthGoal from '../components/ModalHealthGoal';
+import {
+  SafeAreaView,
+  StatusBar,
+  ScrollView,
+  StyleSheet,
+  Platform,
+  Modal,
+  View,
+} from 'react-native';
 
 const HealthGoalsScreen = () => {
-  const [selectedGender, setSelectedGender] = React.useState();
-  const [age, setAge] = React.useState();
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <Text style={styles.inputText} variant="overline">
-          Your age :
-        </Text>
-        <TextInput
-          style={styles.inputLayout}
-          variant="standard"
-          placeholder="Enter your age"
-          leading={(props) => <Icon name="account" {...props} />}
-          onChangeText={(value) => setAge(value)}
-          keyboardType="numeric"
-        />
-        <Text style={styles.inputText} variant="overline">
-          Your gender :
-        </Text>
-        <Picker
-          mode="dialog"
-          style={Platform.OS === 'android' ? styles.inputPickerAndroid : styles.inputPickerIos}
-          selectedValue={selectedGender}
-          onValueChange={(itemValue) => setSelectedGender(itemValue)}>
-          <Picker.Item label="Male" value="male" />
-          <Picker.Item label="Female" value="female" />
-        </Picker>
+  const [age, setAge] = useState();
+  const [gender, setGender] = useState('male');
+  const [height, setHeight] = useState();
+  const [weight, setWeight] = useState();
+  const [activityLevel, setActivityLevel] = useState('sedentary');
+  const [healthGoal, setHealthGoal] = useState('loss');
+  const [modalVisible, setModalVisible] = useState(false);
 
-        <Text style={styles.inputText} variant="overline">
-          Your height :
-        </Text>
-        <TextInput
-          style={styles.inputLayout}
-          variant="standard"
-          placeholder="Enter your height"
-          leading={(props) => <Icon name="human-male-height" {...props} />}
-          keyboardType="numeric"
-        />
-        <Text style={styles.inputText} variant="overline">
-          Your weight :
-        </Text>
-        <TextInput
-          style={styles.inputLayout}
-          variant="standard"
-          placeholder="Enter your weight"
-          leading={(props) => <Icon name="weight" {...props} />}
-          keyboardType="numeric"
-        />
-        <Text style={styles.inputText} variant="overline">
-          Your activity level :
-        </Text>
-        <Picker
-          mode="dialog"
-          style={Platform.OS === 'android' ? styles.inputPickerAndroid : styles.inputPickerIos}
-          selectedValue={selectedGender}
-          onValueChange={(itemValue) => setSelectedGender(itemValue)}>
-          <Picker.Item label="Sedentary" value="sedentary" />
-          <Picker.Item label="Light exercise" value="lightexercise" />
-          <Picker.Item label="Moderate exercise" value="moderateexercise" />
-          <Picker.Item label="Heavy exercise" value="heavyexercise" />
-          <Picker.Item label="Extra active" value="extraactive" />
-        </Picker>
-        <Text style={styles.inputText} variant="overline">
-          Health goal :
-        </Text>
-        <Picker
-          mode="dialog"
-          style={Platform.OS === 'android' ? styles.inputPickerAndroid : styles.inputPickerIos}
-          selectedValue={selectedGender}
-          onValueChange={(itemValue) => setSelectedGender(itemValue)}>
-          <Picker.Item label="Weight loss" value="weightloss" />
-          <Picker.Item label="Weight maintenance" value="weightmaintenance" />
-          <Picker.Item label="Weight gain" value="weightgain" />
-        </Picker>
-        <StatusBar />
-        <Button style={styles.inputText} title="Submit Health Goals" />
-      </ScrollView>
-    </SafeAreaView>
+  getBMRfromform = () => {
+    let bmr;
+    if (gender === 'male') bmr = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age;
+    else bmr = 447.593 + 9.247 * weight + 3.098 * height - 4.33 * age;
+    switch (activityLevel) {
+      case 'sedentary':
+        bmr *= 1.2;
+        break;
+      case 'light':
+        bmr = bmr * 1.375;
+        break;
+      case 'moderate':
+        bmr = bmr * 1.55;
+        break;
+      case 'heavy':
+        bmr = bmr * 1.725;
+        break;
+      case 'extra':
+        bmr = bmr * 1.9;
+        break;
+    }
+    switch (healthGoal) {
+      case 'loss':
+        bmr = bmr * 0.9;
+        break;
+      case 'gain':
+        bmr = bmr * 1.1;
+        break;
+    }
+
+    return bmr;
+  };
+  _submitForm = () => {
+    setModalVisible(true);
+  };
+
+  return (
+    <>
+      <ModalHealthGoal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        bmr={getBMRfromform()}></ModalHealthGoal>
+      <SafeAreaView>
+        <ScrollView>
+          <Text style={styles.inputText} variant="overline">
+            Your age :
+          </Text>
+          <TextInput
+            style={styles.inputLayout}
+            variant="standard"
+            placeholder="Enter your age"
+            leading={(props) => <Icon name="account" {...props} />}
+            onChangeText={(value) => setAge(value)}
+            keyboardType="numeric"
+          />
+          <Text style={styles.inputText} variant="overline">
+            Your gender :
+          </Text>
+          <Picker
+            mode="dialog"
+            style={Platform.OS === 'android' ? styles.inputPickerAndroid : styles.inputPickerIos}
+            selectedValue={gender}
+            onValueChange={(itemValue) => setGender(itemValue)}>
+            <Picker.Item label="Male" value="male" />
+            <Picker.Item label="Female" value="female" />
+          </Picker>
+          <Text
+            style={Platform.OS === 'android' ? styles.inputText : styles.inputTextIos}
+            variant="overline">
+            Your height (in cm):
+          </Text>
+          <TextInput
+            style={styles.inputLayout}
+            variant="standard"
+            onChangeText={(value) => setHeight(value)}
+            placeholder="Enter your height"
+            leading={(props) => <Icon name="human-male-height" {...props} />}
+            keyboardType="numeric"
+          />
+          <Text style={styles.inputText} variant="overline">
+            Your weight (in kg):
+          </Text>
+          <TextInput
+            style={styles.inputLayout}
+            variant="standard"
+            onChangeText={(value) => setWeight(value)}
+            placeholder="Enter your weight"
+            leading={(props) => <Icon name="weight" {...props} />}
+            keyboardType="numeric"
+          />
+          <Text style={styles.inputText} variant="overline">
+            Your activity level :
+          </Text>
+          <Picker
+            mode="dialog"
+            style={Platform.OS === 'android' ? styles.inputPickerAndroid : styles.inputPickerIos}
+            selectedValue={activityLevel}
+            onValueChange={(itemValue) => setActivityLevel(itemValue)}>
+            <Picker.Item label="Sedentary" value="sedentary" />
+            <Picker.Item label="Light exercise" value="light" />
+            <Picker.Item label="Moderate exercise" value="moderate" />
+            <Picker.Item label="Heavy exercise" value="heavy" />
+            <Picker.Item label="Extra active" value="extra" />
+          </Picker>
+          <Text style={styles.inputText} variant="overline">
+            Health goal :
+          </Text>
+          <Picker
+            mode="dialog"
+            style={Platform.OS === 'android' ? styles.inputPickerAndroid : styles.inputPickerIos}
+            selectedValue={healthGoal}
+            onValueChange={(itemValue) => setHealthGoal(itemValue)}>
+            <Picker.Item label="Weight loss" value="loss" />
+            <Picker.Item label="Weight maintenance" value="maintenance" />
+            <Picker.Item label="Weight gain" value="gain" />
+          </Picker>
+          <StatusBar />
+          <Button
+            style={styles.inputText}
+            onPress={this._submitForm}
+            title="Submit Health Goals"
+            disabled={
+              !age ||
+              !(age > 0) ||
+              !(age < 120) ||
+              !weight ||
+              !height ||
+              !(height > 100) ||
+              !activityLevel ||
+              !gender ||
+              !healthGoal
+            }
+          />
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
@@ -90,6 +165,13 @@ const styles = StyleSheet.create({
   inputText: {
     marginBottom: '5%',
     marginTop: '5%',
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+
+  inputTextIos: {
+    marginBottom: '5%',
+    marginTop: '-5%',
     marginLeft: '5%',
     marginRight: '5%',
   },
@@ -102,7 +184,7 @@ const styles = StyleSheet.create({
   },
 
   inputPickerIos: {
-    marginTop: '-20%',
+    marginTop: '-10%',
   },
 
   inputLayout: {
